a63891c250b4d24b65197025c5d28c2b0280fced
mostafa_attiea <mostafaattiea77@gmail.com>
2025-12-08 21:14:26 +0200

    initialize Localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:movie_app/splash/splash_screen.dart';
 import 'package:movie_app/ui/auth/ForgetPassword.dart';
 import 'package:movie_app/api/api_model/movies_response.dart';
@@ -15,6 +14,7 @@ import 'package:movie_app/ui/search_tab/search_tab.dart';
 import 'package:movie_app/utils/app_route.dart';
 import 'package:movie_app/utils/app_theme.dart';
 import 'onBoarding/onboarding_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,19 +28,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.darkTheme,
-
-      // Localization
-      locale: const Locale("en"),
-      supportedLocales: const [
-        Locale("en"),
-        Locale("ar"),
-      ],
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-
       initialRoute: AppRoute.splashScreen,
       routes: {
         AppRoute.splashScreen: (context) => const SplashScreen(),
@@ -64,6 +51,9 @@ class MyApp extends StatelessWidget {
           );
         },
       },
+      locale: Locale("en"),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
     );
   }
 }
